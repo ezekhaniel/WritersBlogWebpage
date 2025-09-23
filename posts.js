58a5e9ef2f063630e029228545d9e0d921e95301
Ezekiel <ezekiel@jeverling.com>
2025-09-22 21:59:56 -0500

    Refactor homepage and posts page to dynamically render content; enhance styles for featured writers links; add live server settings

--- posts.js
+++ posts.js
@@ -1,16 +1,173 @@
-//main.js
-document.addEventListener("DOMContentLoaded", function() {
-    const posts = [
-        { title: "Post Title 1", summary: "Summary of post 1..." },
-        { title: "Post Title 2", summary: "Summary of post 2..." }
-    ];
+// posts.js
+// Expose a small posts dataset and rendering helpers so pages (like index.html)
+// can pull content dynamically.
 
-    const mainSection = document.querySelector("main > section");
+const posts = [
+    {
+        title: "Writing Process Overview!",
+        summary: `
+<p>Many times during my first two years of seriously writing I felt lost, utterly confused and completely idiotic whenever I sat down with my notebook and pen to start writing. 
+I struggled to wrap my mind around forming workable plots, crafting good, relatable characters and even just the writing itself, like, my first partial draft (which I still have😖as a reminder of what NOT to do) was written half in first person and half in third person…don’t even ask how that happened. 
+I have no answer for you other than I’ve learned a lot since then. But whenever I sat down to work on my writing I ended up stopping after just a few minutes in, fully stressed out just from thinking about how much I still had left and feeling ridiculous for the only three or four hundred words I was able to hand write every day. 
+On top of that fact, my parents and family didn’t know I’d started writing and I never was able to take the time to research and learn how to properly write. 
+I never thought about it at the time but a major part of my problem, aside from the obvious lack of narrative skill, my lack of writing skill and access to writing resources in general, and my families exclusion from my attempts, was that I had no outline or guide to help me keep track of everything I created and to help me realize that completing a chapter with a clear end goal in mind was actually getting me somewhere. 
+Thinking about how much I had to do was…quite frankly, overwhelming. 
+Like, there’s so much that goes into a story, how do you know where to even start?? 
+Obviously I did not have any clue where to start as it took another year and a half before I finally managed to determine the difference between narratives and actually finish a full length novel draft and then publish it.🫣 
+So, that is what I’m going to be providing here, an outline to help you do better at writing then I did when I first started. 
+There is a lot to cover so for now I’ll just do a brief overview, then over the next few weeks I’ll go over each piece in depth. 
+I hope you’re able to gather something from this, whether it just be encouragement that no matter how young you are it is possible to do great things. 
+Philippians 4:13 “I can do all things through Christ who strengthens me.” 
+Happy reading!
+</p>
+<h4>Inspiration/Idea</h4>
+<p>
+Every good book starts with a little bit of inspiration, an idea. 
+Where a lot of new writers go wrong is thinking that you have to have a J. R. R. Tolkien level plot, complete with an entire language that you made up yourself. 
+This, while absolutely astonishing if you’re just naturally the next J. R. R. Tolkien Junior and can come up with a complex storyworld without having a brain overload, is not true. 
+No story just comes to an author, themes, character arcs, plots and conflicts all wrapped up neatly in a little package and tied off with a green shiny ribbon. 
+No, oftentimes it’s just a wisp of an idea, floating into your mind after listening to a song, watching a movie, or even just staring out the car window and observing how blue the sky is in that particular moment, and yes, I have done that before. 
+Maybe it’s just a sentence, or a word. 
+It sparks your interest, sets off explosions of excitement and slowly builds into something more. 
+For instance, I was watching Ben-Hur with my family the other day and all of a sudden, during the most riveting, exciting part of the entire movie (the chariot race of course😜) inspiration for a gladiator type book struck. 
+Actually to be specific I think it was right when one of the charioteers tried to run Ben-hur into the wall, my mind went “Oooh, what if I had a character do that but actually succeed in running the other person into the wall.” 🤦‍♀️don’t mind my slightly ruthless brain, it’s fine, I'm fine, I’m just an author not a serial killer. 
+Alright, moving on, basically the point of what I just said was that your idea doesn’t have to be cultivated halfway to China, a simple idea can turn into an amazing story. 
+Your story!
+</p>
+<h4>Preparation</h4>
+<p>
+Preparation is the next step to completing a novel. 
+A lot of people are intimidated by the word because generally prepping stuff isn’t that fun, and takes forever, well, at least most of forever. 
+But in reality, preparation is probably going to be one of the easiest parts of the entire writing process, so easy that there are some people who don’t even do it and just keep everything in their heads…kudos to you if your brain can handle all that and not forget anything, I personally have to make a general outline of my story idea, characters, and storyworld or I’d start having some major problems. 
+Preparation is basically just a lot of outlining, Character outlines, Overall story outlines, Chapter outlines, Story world outlines, making sure that I don’t go insane with how much stuff there is to keep track of outlines…you could make up an outline for literally anything, I know, that seems like a lot and you’re probably starting to feel overwhelmed. 
+Don’t. 
+It looks like a lot, it sounds like a lot, some of it may even be a lot, especially if you’re a perfectionist like me, but it also can be really simple, many of these outlines don’t even take that long, maybe fifteen minutes, and a few of them, depending on your story, aren’t even necessary, actually, technically none of them are necessary but for us people who have forgetful brains and need something to help us keep track of our billions of ideas, outlining is one of the most helpful things in the whole history of writing. 
+Don’t stress about it! 
+When I do another post, going into more detail about preparation, I’ll hopefully have some free, downloadable outline sheets that I use regularly in my own writing to help you along a bit!
+</p>
+<h4>Drafting</h4>
+<p>
+Drafting is the next step to finishing a book. It's the part that everyone loves, as long as they have inspiration to write, and the part everyone dreads when writers block hits and all you want is to have your precious novel published and a physical copy with a beautiful cover in your hands. 
+It's the easiest, yet hardest part of the whole process, and will produce the major portion of tears, stress, happiness, and excitement. 
+Basically, it's the best. 
+Drafting is where you write down your entire novel, without making any edits or revisions whatsoever and don’t stop until every last word of that story you’ve been longing to tell is in existence. 
+This will take a lot of perseverance, consistency, effort and patience. 
+Yes, you read it right, never thought you’d learn a virtue or two while writing did you? 
+I certainly didn’t. 
+I learned it real fast though, as soon as a month passed of my first writing course and I still had nothing on paper because I’d not waited for my teacher's guidance and my chapters were absolutely unworthy of being read, so I ditched them. 
+I strongly recommend to anyone just starting out to go watch a few YouTube videos on writing, or just find a course to sign up for cause without that, I would have been completely lost. 
+This doesn’t mean that if you’ve never watched writing videos or read books about writing or even taken a course on writing that your book draft will be as awful as mine, but it saves so much time in revisions if you don’t just rush headlong into something and don’t have any patience to learn how it’s actually supposed to be done.
+</p>
+<h4>Revision/Editing</h4>
+<p>
+The next step is the revision and editing process, this for some people can be as difficult or more than drafting was. 
+The main reason for that is because you’ve just spent hours upon hours, days upon days, weeks upon weeks, I could keep going up but it’s starting to get old so hopefully you get the point- working on your book, your “child” as many of us authors call it and in revisions your “child” is going to change. 
+A lot. 
+This isn’t true all the time but more often than not, that’s just how it goes, maybe it’s because to fix a plot hole you have to add an entirely new character, somehow incorporate that new person into your MCs life and the events they go through. 
+Maybe it’s taking out a scene because it just sounds like garbage, then when you start the rewrite the entire chapter then needs to be ditched because you had a genius new idea to add. 
+The list continues. 
+People shy away from this because they spent all that time pouring their very souls into that “child” and they don’t want them to change. 
+At all. 
+Even if it’s for the better. 
+It actually is genuinely sad and emotional for them to change it. 
+I get it. 
+It’s hard. 
+I did NOT want to do it either, like, I’d spent five months writing the draft, why in the world would I change it???? 
+Because no matter how good of a writer you are, every first draft needs to be revised, edited and changed. 
+Not just to be changed, but to make it better. 
+My book is better for it. 
+Although, I’ve still got a ways to go before it’ll be anywhere near as good as some of my favorite authors. 
+Your book will be better for it too! (Note: Editing and Revision are two different processes, Revision is usually the first set of changes you make to your book, the major ones where you fix plot holes, take out and add scenes. Editing is generally where you check for grammar and do line edits, smaller things like that.)
+</p>
+<h4>Publication/Marketing</h4>
+<p>
+This is what you’ve been waiting for. 
+Dreaming of. 
+Your book. 
+Published. 
+A physical copy in your hand. 
+Your name on the cover. 
+You’ve spent countless hours toiling over this masterpiece, persevering with undeterrable determination. 
+And it’s finally done. 
+First off, when you reach this point in your writing, you’d better celebrate. 
+Buy some ice cream, cookies, or chocolate. 
+Splurge a little. 
+Pat yourself on the back and scream about it in all caps over chat to your best friend. 
+…don’t look at me like that, trust me, it’s normal, you’ll understand how…freeing it is to go do that, especially when they yell back. 
+And then email me so I can congratulate you on that achievement…in all caps.🤭 
+but seriously, that’s a big accomplishment. 
+You should be proud. 
+Because. 
+You. 
+Did. 
+It. 🥳🥳
+Now that you’ve done all that celebrating it’s time to get serious. 
+You may have finished your book, and gotten it published, but your work's not over. 
+Thousands of books are published regularly, it won’t be long before your book is buried beneath how many others. 
+It’s your job to keep that from happening. 
+How, you may ask? 
+Marketing. 
+Build a blog, an Instagram account, a YouTube channel, even a simple newsletter. 
+Make an effort to go to writers conferences, meet other authors, and spread the word about your book. 
+Don’t be shy. 
+To get your book out there, you have to get out there. 
+So there you are! A brief overview of the writing process! 
+How do you feel? 
+Excited? 
+Slightly hesitant? 
+Hopefully it's the former. 
+I”ll be diving deeper into each step soon so stick around to get more info! 
+Thanks for reading!
+-Megan 🖊️ 🎶 💣😇
+`
+    },
+    { title: "Post Title 2", summary: "Summary of post 2..." }
+];
 
-    // Dynamically add posts to the section
+// expose posts so pages can access them
+window.posts = posts;
+
+function _extractFirstParagraph(html) {
+    const tmp = document.createElement('div');
+    tmp.innerHTML = html;
+    const p = tmp.querySelector('p');
+    return p ? p.innerHTML : tmp.textContent || '';
+}
+
+// Render the first `count` posts into the element matched by selector.
+window.renderLatest = function(selector, count = 1) {
+    const container = document.querySelector(selector);
+    if (!container) {
+        console.warn('posts.js: target selector not found:', selector);
+        return;
+    }
+    container.innerHTML = '';
+    const heading = document.createElement('h1');
+    heading.textContent = 'Latest posts';
+    container.appendChild(heading);
+
+    for (let i = 0; i < Math.min(count, posts.length); i++) {
+        const post = posts[i];
+        const h3 = document.createElement('h3');
+        h3.textContent = post.title;
+        const p = document.createElement('p');
+        p.innerHTML = _extractFirstParagraph(post.summary);
+        container.appendChild(h3);
+        container.appendChild(p);
+    }
+    // append a "Read more" link that leads to the full posts page
+    const moreWrap = document.createElement('p');
+    moreWrap.innerHTML = '<a href="posts.html">Read more &raquo;</a>';
+    container.appendChild(moreWrap);
+};
+
+// Render all posts into a container (useful for posts.html)
+window.renderAllPosts = function(selector) {
+    const container = document.querySelector(selector);
+    if (!container) return;
     posts.forEach(post => {
-        const article = document.createElement("article");
-        article.innerHTML = `<h3>${post.title}</h3><p>${post.summary}</p>`;
-        mainSection.appendChild(article);
+        const article = document.createElement('article');
+        article.innerHTML = `<h3>${post.title}</h3>` + post.summary;
+        container.appendChild(article);
     });
-});
+};
